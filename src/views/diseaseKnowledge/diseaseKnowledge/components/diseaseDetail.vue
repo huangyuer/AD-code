cc5a8c450c19e74fc959cf85c665e2e83baa9aa4
huangyuer <1766701556@qq.com>
2020-03-15 23:19:46 +0800

    update router

---
+++ src/views/diseaseKnowledge/diseaseKnowledge/components/diseaseDetail.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class='diseaseDetail'>
+ssss
+    </div>
+</template>
+<script>
+export default {
+   name:'DiseaseDetail',
+   data() {
+      return {
+          
+      }
+   },
+}
+</script>
+<style lang='less' scoped>
+
+</style>
