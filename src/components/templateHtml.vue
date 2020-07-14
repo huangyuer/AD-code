cb614a3ebe4822fdb3df09339c3ae5128268496c
huangyuer <1766701556@qq.com>
2020-07-14 23:35:23 +0800

    Update templateHtml.vue

--- src/components/templateHtml.vue
+++ src/components/templateHtml.vue
@@ -1,29 +1,41 @@
 <template>
-   <div class="">
-ddddd
-   </div>
+  <div class>
+    <img @click="jump(item.link)" v-for="(item,index) in htmlImg" :key="index" :src="item.url" alt />
+  </div>
 </template>
 
 <script>
 export default {
-   name: 'templateHtml',
-   data() {
-       return {}
-   },
+  name: "templateHtml",
+  data() {
+    return {
+      htmlImg: []
+    };
+  },
 
   components: {},
-  created(){
-      this.$store
-      .dispatch("common/getTempHtml", {id:this.$route.query.id})
+  created() {
+    this.$store
+      .dispatch("common/getTempHtml", { id: this.$route.query.id })
       .then(data => {
-      
+        this.htmlImg = data.html.images;
+        window.document.title = data.html.name;
       })
-      .catch(e => {
-        console.log("=====ss", e);
-      });
+      .catch(e => {});
   },
-}
+  methods: {
+    jump(link) {
+      if (link) {
+        window.open(link, "_blank");
+      }
+    }
+  }
+};
 </script>
 
-<style>
+<style lang="less" scoped>
+img {
+  max-width: 100%;
+  margin-bottom: 0.2rem;
+}
 </style>
